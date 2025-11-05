a6413705ff0c0e6a8436a5603e390f30534f448b
Rafo044 <rafo044@protonmail.com>
2025-11-05 17:06:46 +0400

    Add restore command for pgbackrest

--- backup/src/queries.sql
+++ backup/src/queries.sql
@@ -4,4 +4,5 @@ ALTER SYSTEM SET archive_log_format = '%t_%s_%r.arc';
 ALTER SYSTEM SET max_wal_senders = 10;
 ALTER SYSTEM SET wal_level = replica;
 ALTER SYSTEM SET archive_command = 'pgbackrest --stanza=demo archive-push %p';
+ALTER SYSTEM SET restore_command = 'pgbackrest --stanza=main archive-get %f "%p"';
 SELECT pg_reload_conf();
